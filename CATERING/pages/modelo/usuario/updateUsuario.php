df22d6125331bff8b2f2f78395662b8c879c7587
karentorres06 <kmtomo0604@gmail.com>
2017-02-11 13:30:05 -0500

    actualizado

--- CATERING/pages/modelo/usuario/updateUsuario.php
+++ CATERING/pages/modelo/usuario/updateUsuario.php
@@ -13,53 +13,61 @@ $UsuarioCollectorObj = new UsuarioCollector();
 
 ?>
 
-<DOCTYPE html>
-<html lang ="es">
+<html>
 <head>
-    <meta charset="utf-8">
-	<title>Usuario</title>
-    <link rel="stylesheet" href="../../../css/estilotabla.css">
+	<meta charset="utf-8">
+	<title>Usuario </title>
     <link rel="stylesheet" href="../../../css/bootstrap.css">
     <link rel="stylesheet" href="../../../css/style2.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+          
 </head>
-<body>
-    <header>
-        <h1>Login</h1>
-        <div class="usuario">
-        <?php
-            echo "<p>Hola usuario <span class='nmbUser'>" . $usuario . "<span> </p>";
-            echo "<a href='read_mineral.php'><button>Salir</button></a>";
-		    
-        ?>
-        </div>
-     </header>
-    <div class="contLogin">
-                <h1>MODIFICANDO USUARIO</h1>
-                <form class="form-horizontal" method="post" action="save_updateUsuario.php">
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="../index.html"><h2>MODIFICAR USUARIOS</h2></a>
+  </div>
+
+  <div class="register-box-body">
+    <p class="login-box-msg">Ingrese sus Datos</p>
+
+    <form class="form-horizontal" method="post" action="save_updateUsuario.php">
                       <?php
-                        echo "<label >Id_Usuario</label>";
-
-                        echo "<input type='text' name='id_usuario' value='". $id_usuario ."' readonly>";
-
-                        echo "<br>";
-
-                        echo "<label >Usuario </label>";
-                   
-                    
-                        echo "<input type='text' name='usuario' value='". $usuario ."'>";
-
-                        echo "<br>";
-
-                        echo "<label>Password &nbsp;&nbsp;&nbsp;</label>";
-                        echo "<input type='text' value='". $clave ."' name='clave'>"
-                    ?>
-                    <br><br>
-                    <?php
-                    echo "<a href='save_update_mineral.php?id_mineral=". $id_usuario ."'><button type='submit'>Modificar</button></a>";
+                        echo "<div class='box-body'>
+                                    <div class='form-group'>
+                                        <label for='text' class='col-sm-2 control-label'>ID</label>
+                                        <div class='col-sm-10'>
+                                            <input type='text' class='form-control' id='inputEmail3' name='id_usuario' value='". $id_usuario ."'>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class='form-group'>
+                                        <label for='inputEmail3' class='col-sm-2 control-label'>Usuario</label>
+                                        <div class='col-sm-10'>
+                                            <input class='form-control' id='inputEmail3' type='text' name='usuario' value='". $usuario ."'>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class='form-group'>
+                                        <label for='text' class='col-sm-2 control-label'>Password</label>
+                                        <div class='col-sm-10'>
+                                            <input type='text' class='form-control' id='inputEmail3' name='clave' value='". $clave ."'>
+                                        </div>
+                                    </div>
+                              </div>";
+                       echo "<div class='box-footer'>
+                                <a href='readUsuario.php'><button type='button' class='btn btn-default'>CANCELAR
+                                </button>
+                                </a>
+                                <a href='save_updateUsuario.php?id_usuario=". $id_usuario ."'>
+                                    <button type='submit' class='btn btn-info pull-right'>MODIFICAR</button>
+                                </a>
+                            </div>";
                     ?>
                 </form>
-            </div>
-    </div>
-    
-    </body>
+  </div>
+  <!-- /.form-box -->
+</div>
+</body>
 </html>
