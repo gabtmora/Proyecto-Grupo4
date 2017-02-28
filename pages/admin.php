dd6f8f5e929875562b7df6afabb8f3d0045a2fd2
karentorres06 <kmtomo0604@gmail.com>
2017-02-28 18:50:15 +0100

    ens

--- pages/admin.php
+++ pages/admin.php
@@ -45,7 +45,7 @@
 							<span class="icon-bar"></span>
 							<span class="icon-bar"></span>
 						</button>
-					    <a class="navbar-brand" href="index.html"><i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i><span>C</span>atering</a>
+					    <a class="navbar-brand" href="../index.html"><i class="glyphicon glyphicon-cutlery" aria-hidden="true"></i><span>C</span>atering</a>
 					</div>
 					<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse nav-wil" id="bs-example-navbar-collapse-1">
