fb8eb9d9ce5ce26b57a301531cb88ce272b31e90
gabtmora <gabtmora23@hotmail.com>
2017-03-02 16:59:03 -0500

    cambios

--- Test.php
+++ Test.php
@@ -1,8 +1,18 @@
 <?php
+include_once('/pages/modelo/Plato/PplatoCollector.php');
+
 class Test extends PHPUnit_Framework_TestCase
 {
   public function testOnePlusOne() {
     $this->assertEquals(1+1,2);
     }
+    
+    public function testOnePlusOne2() {
+    	
+    	$DemoCollectorObj = new platoCollector();
+    	$this->assertEquals($menuCollectorObj->showmenus(),array());
+    }
+    
+    
 }
 ?>
